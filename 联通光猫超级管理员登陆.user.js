8cfa7dae64e86f2c80e04973913c73f2542f0039
monsterzzzz <32784687+monsterzzzz@users.noreply.github.com>
2020-07-30 00:10:05 +0800

    get rid of jQuery
    防止无互联网连接时不能使用

--- 联通光猫超级管理员登陆.user.js
+++ 联通光猫超级管理员登陆.user.js
@@ -1,25 +1,26 @@
-
 // ==UserScript==
 // @name         联通光猫超级管理员登陆
 // @namespace    https://github.com/monsterzzzz/UserScript
-// @version      0.1.1
-// @description  目前测试支持光猫 KD-YUN-811E(北京联通) 软件版本V1.1.0
+// @version      0.1.2
+// @description  目前测试支持光猫 KD-YUN-811E(北京联通) 软件版本V1.1.0，其他光猫新的软件版本可以尝试
 // @author       monsterzzzz
 // @match        http://192.168.1.1/*
-// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
 // @grant        unsafeWindow
 // ==/UserScript==
 
 //部分代码来自https://github.com/luomoxu/UserScript
 (function () {
     'use strict';
-    if ($('title').text() == '中国联通家庭网关') {
-        $(".wrapper_administrtor_passwordbox").after('<div class="wrapper_administrtor_passwordbox"><input style="width: 323px;" type="button" id="admin_login" value="管理员登陆" class="wrapper_administrtor_but" onclick=""></div>')
-        $("#admin_login").on("click", function () {
-            $("#username").val('CUAdmin');
-            $("#loginfrm").attr('method','get');
-            $("#loginfrm").submit();
-        });
+    if (document.title == '中国联通家庭网关') {
+        var passwordbox = document.getElementsByClassName("wrapper_administrtor_passwordbox")[0];
+        var adminbutton = document.createElement("div");
+        adminbutton.innerHTML='<div class="wrapper_administrtor_passwordbox"><input style="width: 323px;" type="button" id="admin_login" value="管理员登陆" class="wrapper_administrtor_but" onclick=""></div>';
+        passwordbox.parentNode.insertBefore(adminbutton, passwordbox.nextSibling);
+        adminbutton.onclick = function() {
+            document.getElementById('username').value = 'CUAdmin';
+            document.getElementById('loginfrm').setAttribute('method','get');
+            document.getElementById('loginfrm').submit();
+        };
         console.log(unsafeWindow.authLevel)
         if (typeof unsafeWindow.authLevel != "undefined") {
             unsafeWindow.authLevel = 10;
